964d8f10a8314f0f4995541f01d8e39d5edae51f
malcovys <malcovys22.aps@gmail.com>
2024-12-24 09:27:58 +0300

    remove HouseConfig model and related migrations; add automatise_task_management field to houses table; update DatabaseSeeder to comment out HouseConfigSeeder

--- app/Models/House.php
+++ app/Models/House.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class House extends Model
@@ -27,9 +26,5 @@ class House extends Model
 
     public function users(): HasMany {
         return $this->hasMany(User::class);
-    }
-
-    public function config(): BelongsTo {
-        return $this->BelongsTo(HouseConfig::class);
     }
 }
--- database/migrations/2024_11_09_114017_create_houses_table.php
+++ database/migrations/2024_11_09_114017_create_houses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('emblem_url')->nullable();
+            $table->boolean('automatise_task_management')->default(true);
             $table->timestamps();
         });
     }
